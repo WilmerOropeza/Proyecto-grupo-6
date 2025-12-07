a5c94b35d922e04242c3a886e348fdb0be105132
Wilmer <oropezawilmer38@gmail.com>
2025-12-07 09:42:24 -0400

    Añadir función registrar_entrada (placa y hora)

--- Proyecto.c
+++ Proyecto.c
@@ -16,3 +16,41 @@ void limpiador_buffer(){
 
 }
 
+void registrar_entrada() { 
+    if (contador_registro >= 100){
+        printf("Error: El registro de vehiculo esta lleno.\n");
+        return;
+    
+}
+printf("\n--- Registro de entrada ---\n");
+
+printf ("Ingrese la placa del vehiculo (max. 9 caracteres): ");
+if (scanf("%9s", placas[contador_registro]) != 1) {
+    limpiador_buffer();
+    printf("Error al leer la placa.\n");
+    return;
+
+}
+
+time_t tiempo_actual;
+struct tm *info_tiempo;
+
+tiempo_actual = time(NULL);
+info_tiempo = localtime(&tiempo_actual);
+
+strftime(hora_entrada[contador_registro], 
+    9, 
+    "%H:%M:%S", 
+    info_tiempo);
+
+printf("Vehiculo con placa *%s* registrado a la hora: *%s*\n", 
+        placas[contador_registro], 
+        hora_entrada[contador_registro]);
+
+contador_registro++;
+    
+    limpiador_buffer();
+}
+
+
+
